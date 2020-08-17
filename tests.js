c242a69732cddf1267aa28688bab1ac269a1332a
Sara T <sahara.tijol@gmail.com>
2020-08-17 11:42:09 -0500

    Redo Exercise 3

--- tests.js
+++ tests.js
@@ -21,8 +21,8 @@ describe('sayHello', function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe("string");
     });
-    it('should return the string "Hello, Jane!" when executed', function() {
-        expect(sayHello()).toBe("Hello, Jane!");
+    it('should return the string "" when executed', function() {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
     });
     it("should never return 'undefined' when called", function() {
         expect(sayHello()).not.toBe(undefined);
