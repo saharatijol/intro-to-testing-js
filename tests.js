11c8e5052aa41073aa5ce72f62c4e9c254a573a1
Sara T <sahara.tijol@gmail.com>
2020-08-17 21:48:18 -0500

    Reworked Exercise #2 very carefully

--- tests.js
+++ tests.js
@@ -19,3 +19,9 @@ describe('sayHello', function() {
         expect(typeof sayHello).toBe('function');
     });
 });
+
+describe('sayHello', function() {
+    it ('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+});
