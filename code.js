83c42392d8f0a8c63ba377bcbc02bcdca8cc3c08
Sara T <sahara.tijol@gmail.com>
2020-08-18 00:12:01 -0500

    isEven completed, needs refactoring

--- code.js
+++ code.js
@@ -20,3 +20,15 @@ function isFive(input) {
         return 'boolean';
     }
 }
+
+function isEven(input) {
+    if ((input%2===0 || input === "8") && (input !== null)) {
+        return 'true';
+    }
+    else if (input === 3 || input === "banana" || input === Infinity || input === 'boolean' || input === null) {
+        return 'false';
+    }
+    else {
+        return 'boolean';
+    }
+}
--- tests.js
+++ tests.js
@@ -57,3 +57,36 @@ describe('isFive', function() {
         expect(isFive("5")).toBe('true');
     });
 });
+
+describe('isEven', function() {
+    it ('should be a defined function', function() {
+        expect(typeof isEven).toBe('function');
+    });
+    it ('should return boolean no matter what input', function() {
+        expect(isEven()).toBe('boolean');
+    });
+    it ('should return true if isEven(2)', function() {
+        expect(isEven(2)).toBe('true');
+    });
+    it ('should return true if isEven(-4)', function() {
+        expect(isEven(-4)).toBe('true');
+    });
+    it ('should return false if isEven(3)', function() {
+        expect(isEven(3)).toBe('false');
+    });
+    it ('should return false if isEven("banana")', function() {
+        expect(isEven("banana")).toBe('false');
+    });
+    it ('should return true if isEven("8")', function() {
+        expect(isEven("8")).toBe('true');
+    });
+    it ('should return false if isEven(Infinity)', function() {
+        expect(isEven(Infinity)).toBe('false');
+    });
+    it ('should return false when called with a boolean input', function() {
+        expect(isEven('boolean')).toBe('false');
+    });
+    it ('should return false when called without an argument', function() {
+        expect(isEven(null)).toBe('false');
+    });
+});
